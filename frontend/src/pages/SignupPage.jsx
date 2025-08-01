5b87e5584b16c26ee67c76b4e19a1151e80ee693
sri-nikesh-31 <ayinalasrinikesh@gmail.com>
2025-08-01 23:11:37 +0530

    Update SignupPage.jsx

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -24,7 +24,7 @@ export default function Signup() {
     setSuccess("");
 
     try {
-      const response = await fetch("http://localhost:5000/signup", {
+      const response = await fetch("/api/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ ...formData, isIITIMember, memberType }),
